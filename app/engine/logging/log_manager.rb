3efc2e5e56f5b18bd9aeb064c0299a1de328c194
christopher lee <christopher_lee@rapid7.com>
2011-12-12 11:45:37 -0600

    Added comments to the log manager.

--- app/engine/logging/log_manager.rb
+++ app/engine/logging/log_manager.rb
@@ -1,19 +1,35 @@
+#-------------------------------------------------------------------------------------------------------------------
+# Singleton class that manages logging to the log screen.
+#
+# @author: Christopher Lee, christopher_lee@rapid7.com
+#-------------------------------------------------------------------------------------------------------------------
 class LogManager
 
 	private_class_method :new
 
 	@@instance = nil
 
+	#-------------------------------------------------------------------------------------------------------------------
+	# Initializes the log manager
+	#-------------------------------------------------------------------------------------------------------------------
 	def initialize
-		@nsc_connections = {}
 		@log_lines = []
 	end
 
+	#-------------------------------------------------------------------------------------------------------------------
+	# Singleton initialization
+	#-------------------------------------------------------------------------------------------------------------------
 	def self.instance
    		@@instance = new unless @@instance
     	@@instance
 	end
 
+
+	#-------------------------------------------------------------------------------------------------------------------
+	# Adds a log message
+	#
+	# @param message The log message to add.
+	#-------------------------------------------------------------------------------------------------------------------
 	def add_log_message message
 		formatter = message[0..2]
 		core_message = message[3..message.length-1]
@@ -23,9 +39,10 @@ class LogManager
 		@log_lines << log_message
 	end
 
+	#-------------------------------------------------------------------------------------------------------------------
 	# line starts at 1, so index is always line-1
-	# TODO: If current_line is greater than log length,
-	# refresh the page.
+	# TODO: If current_line is greater than log length refresh the page.
+	#-------------------------------------------------------------------------------------------------------------------
 	def get_messages_since_line current_line
 		message_start_index = current_line - 1
 		if message_start_index >= @log_lines.length-1
@@ -35,10 +52,16 @@ class LogManager
 		end
 	end
 
+	#-------------------------------------------------------------------------------------------------------------------
+	# Returns the full log
+	#-------------------------------------------------------------------------------------------------------------------
 	def get_log_body
 		@log_lines
 	end
 
+	#-------------------------------------------------------------------------------------------------------------------
+	# Returns the current time
+	#-------------------------------------------------------------------------------------------------------------------
 	def get_time_block
 		time_block = '['
 		time_block << Time.now.to_s
