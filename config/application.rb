a008b3934e0ec5addb0f2c21b222210d173df318
christopher lee <christopher_lee@rapid7.com>
2012-01-27 17:31:28 -0600

    Fixed auto load to never use require in project

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,9 @@ module WorkFlowEngineOnRails
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    #config.autoload_paths += %W(#{Rails.root}/lib)
+    config.autoload_paths += %W(#{Rails.root}/lib)
     config.autoload_paths += Dir["#{Rails.root}/app/engine/"]
+    config.autoload_paths += Dir["#{Rails.root}/app/engine/**/"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
