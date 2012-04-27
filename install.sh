cf0ceee6eb59a8c40dc143e6489a2fe485bdc95a
bperry-r7 <brandon_perry@rapid7.com>
2012-04-27 10:45:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,25 +4,15 @@ if [ `whoami` != "root" ]
 then
 	echo "I need to be root in order to install nexflow"
 	echo "Quitting"
-	exit
+	exit 255
 fi
 
 type ruby >> /dev/null
 
 if [ $? -ne 0 ]
 then
-	apt-get install curl
-	bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
-	
-	source /etc/profile.d/rvm.sh
-
-	echo "source /etc/profile.d/rvm.sh" >> ~/.bash_profile
-
-	apt-get install -y libssl-dev build-essential libxslt-dev libpq-dev
-	rvm install 1.9.3
-	rvm use 1.9.3
-	gem install bundler
-	gem install rails
+	echo "Please install ruby."
+	exit 255
 fi
 
 type gem >> /dev/null
@@ -30,7 +20,7 @@ type gem >> /dev/null
 if [ $? -ne 0 ]
 then
 	echo "I need rubygems installed. This should have been installed with RVM."
-	exit 1
+	exit 255
 fi
 
 type bundle >> /dev/null
@@ -38,7 +28,7 @@ type bundle >> /dev/null
 if [ $? -ne 0 ]
 then
 	echo "I need the bundler gem installed (gem install bundler). This should have been installed with RVM."
-	exit 1
+	exit 255
 fi
 
 type rails >> /dev/null
@@ -46,30 +36,36 @@ type rails >> /dev/null
 if [ $? -ne 0 ]
 then
 	echo "I need rails installed. This should have been installed with RVM."
-	exit 1
+	exit 255
 fi
 
 type git >> /dev/null
 
 if [ $? -ne 0 ]
 then
-	apt-get install -y git-core
+	echo "Please install git."
+	exit 255
 fi
 
 apt-get install -y postgresql-9.1 postgresql-contrib-9.1
+
+sed -i 's/5432/5433/' /etc/postgresql/9.1/main/postgresql.conf
+
+/etc/init.d/postgresql restart
+
 echo -e "\n\n\n\n\n\n\nPlease enter the new password for the \"postgres\" user."
-echo "\\password" | sudo -u postgres psql
+echo "\\password" | sudo -u postgres psql -p 5433
 
-git clone git://github.com/rapid7/workflow-client.git /opt/nexflow || exit 1
+git clone git://github.com/rapid7/workflow-client.git /opt/nexflow || exit 255
 
-cd /opt/nexflow || exit 1
+cd /opt/nexflow || exit 255
 
-bundle install || exit 1
+bundle install || exit 255
 
 #not always needed, but have needed in the past. Can't hurt.
-bundle update || exit 1
+bundle update || exit 255
 
-cp config/database.yml.bak config/database.yml || exit 1
+cp config/database.yml.bak config/database.yml || exit 255
 
 echo "What is the postgresql host?"
 read PGHOST
@@ -90,8 +86,11 @@ sed -i "s/username: postgres/username: $PGUSER/" config/database.yml
 sed -i "s/password: msf3Password/password: $PGPASS/" config/database.yml
 sed -i "s/password: password/password: $PGPASS/" config/database.yml
 
-rake db:create || exit 1
-rake db:migrate || exit 1
-rake db:seed || exit 1
+rake db:create || exit 255
+rake db:migrate || exit 255
+rake db:seed || exit 255
+
+echo -e "\n\n\nNexflow is now installed. Change to the /opt/nexflow directory and begin the application with:"
+echo -e "\n\tcd /opt/nexflow && rails server webrick -e development -p 3000"
 
 exit 0
